a4b7b915a2a85de59ea48e89066b9adc9fce5278
jay <chairit34@gmail.com>
2020-11-23 15:57:09 +0700

    fix user register code duplicate

--- controllers/user.js
+++ controllers/user.js
@@ -19,34 +19,6 @@ const register = async (req, res) => {
   if (!email) {
     return res.status(400).send({ error: "no email provided" });
   }
-  if (targetUser) {
-    console.log("err");
-    res.status(400).send({ message: "User already taken" });
-  } else {
-    console.log("register");
-    const salt = bcryptjs.genSaltSync(12);
-    const hashedPassword = bcryptjs.hashSync(password, salt);
-
-    await db.User.create({
-      name,
-      email,
-      birthday,
-      gender,
-      target,
-      lat,
-      long,
-      motto,
-      password: hashedPassword,
-      s,
-    });
-    const newUser = await db.User.findOne({ where: { email } });
-    console.log(newUser.id);
-    if (imageUrl) {
-      await db.Photo.create({ imageUrl, user_id: newUser.id });
-    }
-
-    res.status(201).send({ message: "User created" });
-  }
 
   const targetUser = await db.User.findOne({ where: { email } });
 
@@ -85,22 +57,21 @@ const login = async (req, res) => {
   const { email, password } = req.body;
   const targetUser = await db.User.findOne({ where: { email } });
 
-    if (targetUser) {
-      if (bcryptjs.compareSync(password, targetUser.password)) {
-        const token = jwt.sign(
-          { id: targetUser.id, name: targetUser.name },
-          "datingApp",
-          { expiresIn: 3600 }
-        );
-        const role = targetUser.role 
-        res.status(200).send({ token, role });
-      } else {
-        res.status(400).send({ message: "Wrong password" });
-      }
+  if (targetUser) {
+    if (bcryptjs.compareSync(password, targetUser.password)) {
+      const token = jwt.sign(
+        { id: targetUser.id, name: targetUser.name },
+        "datingApp",
+        { expiresIn: 3600 }
+      );
+      const role = targetUser.role;
+      res.status(200).send({ token, role });
     } else {
-      res.status(400).send({ message: "Not found account" });
+      res.status(400).send({ message: "Wrong password" });
     }
-
+  } else {
+    res.status(400).send({ message: "Not found account" });
+  }
 };
 
 module.exports = { register, login };
