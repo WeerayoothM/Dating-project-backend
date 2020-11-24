63196cfffe16c2c4479bbec9c34b94fcf4d246e5
jay <chairit34@gmail.com>
2020-11-24 14:20:17 +0700

    fix dotenv config file

--- controllers/cloudinary.js
+++ controllers/cloudinary.js
@@ -1,27 +1,27 @@
-const Formidable = require('formidable');
+const Formidable = require("formidable");
 const cloudinary = require("cloudinary");
 
 cloudinary.config({
-    cloud_name: 'software-park-thailand',
-    api_key: '926694112918671',
-    api_secret: 'q9V_gK8DTstkbd0rJatjbC5adEc'
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
 });
 
 const uploadImage = async (req, res) => {
-    try {
-        const fileStr = req.body.data;
-        // console.log('uploading', fileStr)
-        const uploadResponse = await cloudinary.uploader.upload(fileStr, {
-            upload_preset: 'dev_setups',
-        });
-        console.log(uploadResponse.url);
-        res.status(201).json({ url: uploadResponse.url });
-    } catch (err) {
-        console.error('error', err);
-        res.status(500).json({ err: 'Something went wrong' });
-    }
+  try {
+    const fileStr = req.body.data;
+    // console.log('uploading', fileStr)
+    const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+      upload_preset: "dev_setups",
+    });
+    console.log(uploadResponse.url);
+    res.status(201).json({ url: uploadResponse.url });
+  } catch (err) {
+    console.error("error", err);
+    res.status(500).json({ err: "Something went wrong" });
+  }
 };
 
 module.exports = {
-    uploadImage
+  uploadImage,
 };
